78b046eef5bcca10994be13474bc2db53c18108f
何梦芸 <campanulamediuml@gmail.com>
2025-10-08 20:29:02 +0800

    添加基础工作流插件

--- sirion_plugin_packages/transform/avg_plugin/adapter.py
+++ sirion_plugin_packages/transform/avg_plugin/adapter.py
@@ -1,3 +1,14 @@
 # !/usr/bin/env python
 # -*-coding:utf-8 -*-
 # Author     ：Campanula 梦芸 何
+from typing import Dict, Any
+
+from SirionDep.sirion_dep_frame.plugin_frame import PluginBase
+
+
+class AvgAdapter(PluginBase):
+    def __init__(self, global_config: Dict[str, Any]):
+        super().__init__(global_config)
+
+
+
